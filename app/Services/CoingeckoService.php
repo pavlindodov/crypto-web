7efd44fbdca16e4ecd1c8a618eb5241b3631c5d0
Pavlin Dodov <paulins.dod@educa.jcyl.es>
2025-01-15 20:06:44 +0100

    Implementación Cache Api

--- app/Services/CoingeckoService.php
+++ app/Services/CoingeckoService.php
@@ -2,6 +2,7 @@
 namespace App\Services;
 
 use GuzzleHttp\Client;
+use Illuminate\Support\Facades\Cache;
 
 class CoinGeckoService
 {
@@ -18,26 +19,29 @@ class CoinGeckoService
     public function getTopCryptos()
     {
         try {
-            // Construimos la URL completa, incluyendo la clave API como parte de la query string
-            $url = $this->apiUrl . 'coins/markets';
+            return Cache::remember('top_cryptos', now()->addMinutes(60), function () {
+                // Construimos la URL completa, incluyendo la clave API como parte de la query string
+                $url = $this->apiUrl . 'coins/markets';
 
-            $response = $this->client->get($url, [
-                'query' => [
-                    'x_cg_demo_api_key' => $this->apiKey, // Incluir la clave demo en la query string
-                    'vs_currency' => 'usd',
-                    'order' => 'market_cap_desc',
-                    'per_page' => 100,
-                    'page' => 1,
-                ],
-                'headers' => [
-                    'accept' => 'application/json',
-                ],
-            ]);
+                $response = $this->client->get($url, [
+                    'query' => [
+                        'x_cg_demo_api_key' => $this->apiKey, // Incluir la clave demo en la query string
+                        'vs_currency' => 'usd',
+                        'order' => 'market_cap_desc',
+                        'per_page' => 100,
+                        'page' => 1,
+                    ],
+                    'headers' => [
+                        'accept' => 'application/json',
+                    ],
+                ]);
 
-            return json_decode($response->getBody()->getContents(), true);
+                return json_decode($response->getBody()->getContents(), true);
+            });
         } catch (\Exception $e) {
             // Si ocurre un error, retornamos un arreglo vacío
             return [];
         }
     }
+
 }
