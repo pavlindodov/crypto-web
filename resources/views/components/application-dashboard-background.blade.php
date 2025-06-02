a96f3b7c6127dcd0ddd17e8bbc4377ecc901f0c2
Pavlin Dodov <paulins.dod@educa.jcyl.es>
2025-06-02 19:21:30 +0200

    Fix dashboard background opacity

--- resources/views/components/application-dashboard-background.blade.php
+++ resources/views/components/application-dashboard-background.blade.php
@@ -1,1 +1,1 @@
-<img id="background" class="fixed w-full opacity-10 pointer-events-none z-0" src="{{ asset('images/background.png') }}" alt="dashboard-background" />
+<img id="background" class="fixed w-full opacity-50 pointer-events-none z-0" src="{{ asset('images/background.png') }}" alt="dashboard-background" />
